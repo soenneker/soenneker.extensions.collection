c49359040f13f0fcd35a418312e8e2299014a580
Jake Soenneker <jake@soenneker.com>
2024-06-13 20:25:37 -0500

    AddIfNotNull added

--- src/CollectionExtension.cs
+++ src/CollectionExtension.cs
@@ -9,11 +9,11 @@ namespace Soenneker.Extensions.Collection;
 public static class CollectionExtension
 {
     /// <summary>
-    /// Simple foreach over toRemove list, removing each from the target. This method is safe; no exceptions will be thrown if the collection or the enumerable is null or empty.
+    /// Simple foreach over toRemove collection, removing each from the target. This method is safe; no exceptions will be thrown if the collection or the enumerable is null or empty.
     /// </summary>
-    public static void RemoveEnumerableFromCollection<T>(this ICollection<T>? list, IEnumerable<T>? toRemove)
+    public static void RemoveEnumerableFromCollection<T>(this ICollection<T>? collection, IEnumerable<T>? toRemove)
     {
-        if (list.IsNullOrEmpty())
+        if (collection.IsNullOrEmpty())
             return;
 
         if (toRemove.IsNullOrEmpty())
@@ -21,13 +21,27 @@ public static class CollectionExtension
 
         foreach (T item in toRemove)
         {
-            list.Remove(item);
+            collection.Remove(item);
         }
     }
 
     /// <inheritdoc cref="RemoveEnumerableFromCollection{T}"/>
-    public static void RemoveFromCollection<T>(this ICollection<T> list, params T[] toRemove)
+    public static void RemoveFromCollection<T>(this ICollection<T> collection, params T[] toRemove)
     {
-        RemoveEnumerableFromCollection(list, toRemove);
+        RemoveEnumerableFromCollection(collection, toRemove);
+    }
+
+    /// <summary>
+    /// Adds an item to the collection if the item is not null.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    /// <param name="collection">The collection to add the item to.</param>
+    /// <param name="item">The item to add to the collection if it is not null.</param>
+    public static void AddIfNotNull<T>(this ICollection<T> collection, T? item)
+    {
+        if (item == null)
+            return;
+
+        collection.Add(item);
     }
 }
