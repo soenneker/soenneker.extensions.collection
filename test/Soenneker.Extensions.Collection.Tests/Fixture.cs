60d008c2e946a2ac4ba469b4d77e23301f7212b3
Jake Soenneker <jake@soeneker.com>
2024-12-18 19:34:49 +0000

    xUnit v3

--- test/Soenneker.Extensions.Collection.Tests/Fixture.cs
+++ test/Soenneker.Extensions.Collection.Tests/Fixture.cs
@@ -8,7 +8,7 @@ namespace Soenneker.Extensions.Collection.Tests;
 
 public class Fixture : UnitFixture
 {
-    public override System.Threading.Tasks.Task InitializeAsync()
+    public override System.Threading.Tasks.ValueTask InitializeAsync()
     {
         SetupIoC(Services);
 
