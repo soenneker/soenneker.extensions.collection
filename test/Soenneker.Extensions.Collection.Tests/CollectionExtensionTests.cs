91c76cca6b1b9494fc77322c6bbf7e14f0f65b92
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:49 -0500

    TUnit conversion

--- test/Soenneker.Extensions.Collection.Tests/CollectionExtensionTests.cs
+++ test/Soenneker.Extensions.Collection.Tests/CollectionExtensionTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Extensions.Collection.Tests;
 
 public class CollectionExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
